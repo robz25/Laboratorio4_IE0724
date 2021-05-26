7c09dc1c92dbaee6ec7eadca8a160e1035f7d725
Emilio Rojas <emilio93@gmail.com>
2021-05-26 17:26:20 -0600

    Cambiar nombres de pruebas para que sean más descriptivas

--- test/dummy.cpp
+++ test/dummy.cpp
@@ -6,19 +6,19 @@
 // Prueba negativa:
 // Se ejecutarán las funciones para generar el diagrama de Voronoi con entradas
 // inválidas, y corroborar que las funciones devuelvan un código de error. Se
-// debe usar: 1-Lista de tamaño inválido (lista sin números para Voronoi, y
-// lista con menos de 3 números para Convex Hull).
-TEST(Prueba3, item1) {
+// debe usar:
+// 1-Lista de tamaño inválido (lista sin números).
+TEST(Prueba3, tamano_invalido) {
   EXPECT_THROW(CGALBridge::crearDiagramaVoronoi("./puntosInvalidos1.txt", true),
                std::invalid_argument);
 }
-// 2-Lista con números inválidos: caracteresno numéricos.
-TEST(Prueba3, item2) {
+// 2-Lista con números inválidos: caracteres no numéricos.
+TEST(Prueba3, caracteres_no_numericos) {
   EXPECT_THROW(CGALBridge::crearDiagramaVoronoi("./puntosInvalidos2.txt", true),
                std::invalid_argument);
 }
 // 3-Lista con puntos inválidos: puntos sin el formato aceptado.
-TEST(Prueba3, item3) {
+TEST(Prueba3, numeros_sin_formato_valido) {
   EXPECT_THROW(CGALBridge::crearDiagramaVoronoi("./puntosInvalidos3.txt", true),
                std::invalid_argument);
 }
