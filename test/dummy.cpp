22b172725ebf2132504eda50056e907cfa93e7c1
Robin Gonzalez <weiserfi@outlook.com>
2021-05-27 00:51:05 -0600

    prubeas con el test positivo

--- test/dummy.cpp
+++ test/dummy.cpp
@@ -2,12 +2,39 @@
 
 #include "Util.hpp"
 
+#include "Punto.hpp"
+#include "CGALBridge.hpp"
+#include <time.h>
+#include <fstream>
+#include <iostream>
 
 // Positive testing: use valid input parameter: status should be 0
 TEST(Util, positive) {
     int status = false;
     status = Util::isInt("5");
     EXPECT_EQ(status, true);
+}
+
+TEST(crearDiagramaVoronoi, positive){
+
+	int result = 0;
+
+	std::ofstream archivoPuntos;
+        archivoPuntos.open("puntosTest.txt");
+        float limInferior = -100;
+        float limSuperior = 100;
+        srand (time(NULL));
+        int cantPuntos = rand()%100 +3;
+        for(int c = 0;c < cantPuntos;c++){
+                float x = float(rand())/float((RAND_MAX))*(limSuperior - limInferior) + limInferior;
+                float y = float(rand())/float((RAND_MAX))*(limSuperior - limInferior) + limInferior;
+                archivoPuntos << std::to_string(x) + " "+std::to_string(y)+"\n";
+        }
+        archivoPuntos.close();
+
+	VD voronoi = CGALBridge::crearDiagramaVoronoi("puntosTest.txt");
+
+	EXPECT_EQ(result, 0);	
 }
 
 int main(int argc, char **argv) {
